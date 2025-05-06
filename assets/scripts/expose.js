95afab82d4d67d651d5c7d0f604832afd6cd0060
j5995 <wangtianyi63@gmail.com>
2025-05-06 01:13:47 -0700

    horn image is displayed when horn is changed, need to change loaded audio file

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,10 +3,29 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const volume = document.getElementById("volume")
+  const volume = document.getElementById("volume");
+  const horn = document.getElementById("horn-select");
+  horn.addEventListener("change", updateHorn);
   volume.addEventListener("input", updateVolumeImg);
 
   // TODO
+}
+
+function updateHorn(){
+  const hornChoice = document.getElementById("horn-select").value;
+  const hornImg = document.querySelector("#expose > img");
+  if(hornChoice=="air-horn"){
+    hornImg.src="assets/images/air-horn.svg";
+  }
+  else if(hornChoice=="car-horn"){
+    hornImg.src="assets/images/car-horn.svg";
+  }
+  else if(hornChoice=="party-horn"){
+    hornImg.src="assets/images/party-horn.svg";
+  }
+  else{
+    hornImg.src="assets/images/no-image.png";
+  }
 }
 
 function updateVolumeImg(){
@@ -16,7 +35,6 @@ function updateVolumeImg(){
   audioVal.volume = Number(volumeVal)/100;
   if(volumeVal==="0"){
     volumeImg.src="assets/icons/volume-level-0.svg";
-    console.log(volumeImg.src);
   }
   else if(volumeVal<=33){
     volumeImg.src="assets/icons/volume-level-1.svg";
