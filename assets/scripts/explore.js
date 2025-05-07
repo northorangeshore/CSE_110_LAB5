c8c444dd5032bd9b62c6c7129a94a2b128e8fc49
j5995 <wangtianyi63@gmail.com>
2025-05-06 21:35:51 -0700

    Creates list of voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,30 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const talkButton = document.querySelector("#explore > button");
+  const selectButton = document.getElementById("voice-select");
+  populateVoices();
+  selectButton.addEventListener("click", populateVoices);
+  talkButton.addEventListener("click", playTalk);
   // TODO
+}
+
+function populateVoices(){
+  let voices = [];
+  voices = window.speechSynthesis.getVoices();
+  for(const voice of voices){
+    const option = document.createElement("option");
+    option.textContent = `${voice.name}`;
+    document.getElementById("voice-select").appendChild(option);    
+  }
+  console.log
+}
+
+function playTalk(){
+  const talkImg = document.querySelector("#explore > img");
+  talkImg.src = "assets/images/smiling-open.png";
+//talk
+  console.log(document.getElementById("option"))
+  talkImg.src = "assets/images/smiling.png";
+
 }
